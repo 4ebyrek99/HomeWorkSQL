4c7ae78bf9864a0cc6f8a40a7c085b45d4cc4c2f
4ebyrek <vasya-korn71@mail.ru>
2022-04-25 20:32:02 +0600

    +jwt verify file

---
+++ utils/jwt.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = checkToken = (req, res, next) =>{
+    const token = req.cookies.jwt
+    if(token !== undefined){
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) =>{
+            if(err){
+                res.json({
+                    success: false,
+                    msg: "Вы не вошли в аккаунт!"
+                })
+            }else{
+                next()
+            }
+        })
+    }else{
+        res.json({
+            success: false,
+            msg: "Вы не вошли в аккаунт!"
+        })
+    }
+}
